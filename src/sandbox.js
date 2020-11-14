e90ab6b3f005a2f43b022f5e6c36dfd910c1e2d0
thedersen <thedersen@gmail.com>
2020-11-14 14:29:31 +0100

    Enable bundling in sandbox with --bundle arg

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,11 +1,24 @@
+const rollup = require('rollup');
+const path = require('path');
 const sandbox = require('@architect/sandbox');
+const {updater} = require('@architect/utils');
 const browserSync = require('browser-sync');
 const esInstall = require('./es-install.js');
 
-module.exports = async function () {
+async function bundleModule(module) {
+  const input = path.join(process.cwd(), 'src', 'views', 'modules', module);
+  const bundle = await rollup.rollup({input});
+  const bundled = await bundle.generate({format: 'esm'});
+
+  return bundled.output[0].code;
+}
+
+module.exports = async function ({bundle}) {
+  const log = updater('ProgressiveBundling');
+
   await sandbox.start();
 
-  browserSync.create().init({
+  const options = {
     open: false,
     ui: false,
     files: ['public/**/*', 'src/**/*.js'],
@@ -15,12 +28,37 @@ module.exports = async function () {
       'src/views/package.json',
     ],
     proxy: `localhost:${process.env.PORT}`,
-    serveStatic: [
+  };
+  if (bundle) {
+    options.middleware = [
+      {
+        route: '/_modules',
+        handle(request, response) {
+          const mod = request.url.slice(1); // Remove leading /
+          log.start(`Bundling ${mod}`);
+          bundleModule(mod).then((source) => {
+            log.done(`Bundled ${mod}`);
+            response.writeHead(200, {'Content-Type': 'text/javascript'});
+            response.write(source);
+            response.end();
+          });
+        },
+      },
+    ];
+  } else {
+    options.serveStatic = [
       {
         route: '/_modules',
         dir: ['src/views/modules'],
       },
-    ],
+    ];
+  }
+
+  browserSync.create().init(options, () => {
+    console.log(); // Add an empty line
+    if (bundle) {
+      log.status('Enabled Progressive Bundling');
+    }
   });
 
   browserSync
